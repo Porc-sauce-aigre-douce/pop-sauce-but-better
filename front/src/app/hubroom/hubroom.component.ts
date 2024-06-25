bfa80190c3950d9136f6c3eb187d574d05afbfad
Nathan Piraux <61149320+nathomas34@users.noreply.github.com>
2024-06-25 10:11:56 +0200

    room hub

---
+++ front/src/app/hubroom/hubroom.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hubroom',
+  templateUrl: './hubroom.component.html',
+  styleUrls: ['./hubroom.component.css']
+})
+export class HubroomComponent {
+
+}
