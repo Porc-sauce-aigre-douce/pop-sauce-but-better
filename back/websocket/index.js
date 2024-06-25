44f3d6c1b165d8597e6756e6d17652589d10fa3b
Jeremy Bilger <64845106+VioletThe25th@users.noreply.github.com>
2024-06-25 10:39:27 +0200

    Handle game logic : Start quiz with questions and answers and add points

--- back/websocket/index.js
+++ back/websocket/index.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 8080;
 const ADMIN = 'Admin';
 const app = express();
 
+const rooms = {};
+
 app.use(express.static(path.join(__dirName, "public")));
 
 const expressServer = app.listen(PORT, () => {
@@ -32,6 +34,26 @@ const io = new Server(expressServer, {
     }
 })
 
+const questions = [
+    {questionQuiz: `D'où vient la citation "Le roi des pirates, ce sera moi !"` , answers: ``, correctAnswers: `One Piece`},
+    {questionQuiz: `Comment s'appelle le personnage principal de One Piece ?`, answers: ``, correctAnswers: `Luffy`}
+];
+
+function generateQuestion(no) {
+    const questions = [
+        {question: `D'où vient la citation "Le roi des pirates, ce sera moi !` , answers: ``, correctAnswers: `One Piece`},
+        {question: `Comment s'appelle le personnage principal de One Piece ?`, answers: ``, correctAnswers: `Luffy`}
+    ];
+
+    const selectedQuestions = [];
+    for (let i = 0; i < no; i++) {
+        const randomIndex = Math.floor(Math.random() * questions.length);
+        selectedQuestions.push(questions[randomIndex]);
+    }
+    console.log('question : ', selectedQuestions);
+    return selectedQuestions;
+}
+
 io.on('connection', socket => {
     console.log(`User ${socket.id} connected`);
 
@@ -40,19 +62,20 @@ io.on('connection', socket => {
     })
 
     // User creates a new room
-    socket.on('createRoom', ({name, room, subject}) => {
+    socket.on('createRoom', ({name, roomName, subject}) => {
         if (!name) {
             name = generateGuestName();
         }
-        const roomExists = getAllActiveRooms().includes(room);
-        if (roomExists) {
-            socket.emit('error', buildMsg(ADMIN, `Room ${room} already exists`));
+        if (roomExist(roomName)) {
+            socket.emit('error', buildMsg(ADMIN, `Room ${roomName} already exists`));
         } else {
-            socket.join(room);
-            activateUser(socket.id, name, room);
-            io.to(room).emit('roomUsers', {
-                room: room,
-                users: getUsersInRoom(room),
+            rooms[roomName] = { users: [], quizStarted: false};
+            socket.join(roomName);
+            activateUser(socket.id, name, roomName);
+            socket.emit(`roomCreated`, roomName);
+            io.to(roomName).emit('roomUsers', {
+                room: roomName,
+                users: getUsersInRoom(roomName),
                 subject: subject
             });
             // Update room lists for everyone
@@ -63,35 +86,36 @@ io.on('connection', socket => {
     });
 
     // User joins a room
-    socket.on('joinRoom', ({name, room, subject}) => {
+    socket.on('joinRoom', ({name, roomName, subject}) => {
         if (!name) {
             name = generateGuestName();
         }
-        const roomExists = getAllActiveRooms().includes(room);
-        if (roomExists) {
-            const user = activateUser(socket.id, name, room);
-            socket.join(user.room);
-            io.to(user.room).emit('roomUsers', {
-                room: room,
-                users: getUsersInRoom(room),
+        if (roomExist(roomName)) {
+            const user = activateUser(socket.id, name, roomName);
+            socket.join(roomName);
+            console.log(`User ${socket.id} joined room : ${roomName}`);
+            io.to(roomName).emit('userJoined', socket.id)
+            io.to(roomName).emit('roomUsers', {
+                room: roomName,
+                users: getUsersInRoom(roomName),
                 subject: subject
             })
             // To uer who joined room
-            socket.emit('message', buildMsg(ADMIN, `You joined the room ${user.room}`))
+            socket.emit('message', buildMsg(ADMIN, `You joined the room ${roomName}`), roomName)
             // To everyone else
-            socket.broadcast.to(user.room).emit('message', buildMsg(ADMIN, `${user.name} joined room;`))
+            socket.broadcast.to(roomName).emit('message', buildMsg(ADMIN, `${roomName} joined room;`), roomName)
         } else {
-            socket.emit('error', buildMsg(ADMIN, `${room} not found`));
+            socket.emit('error', buildMsg(ADMIN, `${roomName} not found`));
         }
     })
 
     // User quit room
-    socket.on('quitRoom', ({name}) => {
-        const prevRoom = getUser(socket.id)?.room;
+    socket.on('quitRoom', ({username, roomName}) => {
+        const prevRoom = roomName;
         if (prevRoom) {
             socket.leave(prevRoom);
             // Tell the users in a room that someone leaved
-            io.to(prevRoom).emit('message', buildMsg(ADMIN, `${name} leaved room`));
+            io.to(prevRoom).emit('message', buildMsg(ADMIN, `${username} leaved room`), roomName);
             // Cannot update previous room users list after the state update in activate user
             io.to(prevRoom).emit('userList', {
                 users: getUsersInRoom(prevRoom)
@@ -99,32 +123,55 @@ io.on('connection', socket => {
         }
     })
 
+    socket.on('startQuiz', ({roomName}) => {
+        const randomIndex = Math.floor(Math.random() * questions.length);
+        const selectedQuestions = questions[randomIndex];
+        const question = selectedQuestions.questionQuiz;
+        const answers = selectedQuestions.answers;
+        const correctAnswers = selectedQuestions.correctAnswers;
+
+        console.log('question : ', selectedQuestions);
+        io.to(roomName).emit('question', {question})
+
+        // Listen to user responses :
+        socket.on('submitAnswer', ({userAnswer}) => {
+            const user = getUser(socket.id)
+            user.points = 0;
+            if (user) {
+                if (isAnswerCorrect(userAnswer, correctAnswers)) {
+                    user.points += 1000;
+                    io.to(roomName).emit('correctAnswer', {user});
+                } else {
+                    io.to(roomName).emit('incorrectAnswer', {user});
+                }
+            }
+        })
+    })
+
     // Listening or message event
-    socket.on('message', ({name, text}) => {
-        const room = getUser(socket.id)?.room;
-        if (room) {
-            io.to(room).emit('message', buildMsg(name, text));
+    socket.on('message', ({name, text, roomName}) => {
+        if (roomExist(roomName)) {
+            io.to(roomName).emit('message', buildMsg(name, text));
         }
     })
 
     // Listening for activity
-    socket.on('activity', (name) => {
-        const room = getUser(socket.id)?.room;
-        if (room) {
-            socket.broadcast.to(room).emit('activity', name);
+    socket.on('activity', (name, roomName) => {
+        if (roomExist(roomName)) {
+            socket.broadcast.to(roomName).emit('activity', name);
         }
     })
 
-    socket.on('disconnect', () => {
+    socket.on('disconnect', (roomName) => {
         const user = getUser(socket.id);
         userLeavesApp(socket.id);
         if (user) {
-            io.to(user.room).emit('message',
-                buildMsg(ADMIN, `${user.name} disconnected`));
-            io.to(user.room).emit('userList', {
-                users: getUsersInRoom(user.room)
+            io.to(roomName).emit('message',
+                buildMsg(ADMIN, `${user.name} disconnected`), roomName);
+            io.to(roomName).emit('userList', {
+                users: getUsersInRoom(roomName)
             })
-            io.to(user.room).emit('roomList', {
+            io.to(roomName).emit('roomList', {
                 rooms: getAllActiveRooms()
             })
         }
@@ -153,6 +200,14 @@ function buildMsg(name, text) {
     }
 }
 
+function buildQuestion(question, answers, correctAnswers) {
+    return {
+        question,
+        answers,
+        correctAnswers
+    }
+}
+
 // USERS FUNCTIONS
 
 function activateUser(id, name, room) {
@@ -174,14 +229,27 @@ function getUser(id) {
     return UsersState.users.find(user => user.id === id);
 }
 
-function getUsersInRoom(room) {
-    return UsersState.users.filter(user => user.room === room);
+function getUsersInRoom(roomName) {
+    if (rooms[roomName]) {
+        return rooms[roomName].users;
+    }
+    return null;
 }
 
 function getAllActiveRooms() {
-    return Array.from(new Set(UsersState.users.map(user => user.room)));
+    return rooms
 }
 
 function generateGuestName() {
     return `Guest_${Math.floor(Math.random() * 10000)}`;
+}
+
+function isAnswerCorrect(userAnswer, correctAnswer) {
+    if (userAnswer === correctAnswer) return true
+}
+
+function roomExist(roomName) {
+    if (rooms[roomName]) {
+        return true;
+    }
 }
