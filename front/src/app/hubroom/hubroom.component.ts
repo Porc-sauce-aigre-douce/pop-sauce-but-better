cbe24cf7fc8b11ab515c5938ff0e6b4db49dc25e
Evan <63302524+Evan0911@users.noreply.github.com>
2024-06-27 14:56:23 +0200

    add list of rooms in index

--- front/src/app/hubroom/hubroom.component.ts
+++ front/src/app/hubroom/hubroom.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
-
-import { RouterLink } from '@angular/router';
+import { WebsocketService } from '../services/websocket.service';
 
 @Component({
   selector: 'app-hubroom',
   templateUrl: './hubroom.component.html',
-  styleUrls: ['./hubroom.component.css']
+  styleUrls: ['./hubroom.component.css'],
 })
 export class HubroomComponent {
+  question: any;
+  socket: any;
+  rooms: any[] = [];
 
+  constructor(private websocketService: WebsocketService) {}
+
+  ngOnInit(): void {
+    this.socket = this.websocketService.connect();
+    this.setEvents();
+  }
+
+  setEvents() {
+    this.socket.on('connect', this.onConnect);
+    this.socket.on('disconnect', this.onDisconnect);
+    this.socket.on('roomList', ({ rooms }: any) => {
+      this.rooms = [];
+      for (const [key, value] of Object.entries(rooms)) {
+        console.log(value);
+        if (typeof value === 'object') {
+          this.rooms.push({ ...value, roomName: key });
+        }
+      }
+    });
+    this.socket.on('connect_error', (err: any) => {
+      console.log(`connect_error due to ${err.message}`);
+    });
+    this.socket.on('error', (error: any) => {
+      console.log(error.name + ' ' + error.text + ' ' + error.time);
+    });
+  }
+
+  onConnect(): void {}
+
+  onDisconnect(): void {
+    console.log('disconnected');
+  }
+
+  getUserNumberInRoom(room: any) {
+    return room.users.length;
+  }
 }
