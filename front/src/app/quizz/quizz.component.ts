eaceafc16d78acfd66fa822a6c72be13861a0ed8
Evan <63302524+Evan0911@users.noreply.github.com>
2024-06-28 09:52:04 +0200

    add docker

--- front/src/app/quizz/quizz.component.ts
+++ front/src/app/quizz/quizz.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { QuestionService } from '../services/question.service';
 import { WebsocketService } from '../services/websocket.service';
 import { ActivatedRoute } from '@angular/router';
 
